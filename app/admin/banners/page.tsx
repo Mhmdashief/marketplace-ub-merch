bab053e0776aa7ae95c61f5e0bf8d0b4e5e70be7
Mochamad Iftichor Al Ashief <mhmdashief@gmail.com>
2026-04-28 14:04:23 +0700

    cleanup removed files

--- app/admin/banners/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getBanners } from '@/app/actions/banners';
-import BannersClient from './BannersClient';
-
-export default async function BannersPage() {
-    const banners = await getBanners();
-    return <BannersClient initialBanners={banners} />;
-}
